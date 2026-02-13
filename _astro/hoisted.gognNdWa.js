b7196e4a3b71697e4a89a77c3f41703f3cd392c5
2kindlebox <2kindlebox@gmail.com>
2026-02-13 01:50:30 +0000

    Deploying to gh-pages from  @ 68de6840217c9add101389dceb2319cc72ff8dd3 🚀

--- _astro/hoisted.gognNdWa.js
+++
@@ -1,1 +0,0 @@
-import"./hoisted.Br6xBnpH.js";import"./auth.BGhJ2n4j.js";fetch("https://v1.hitokoto.cn").then(t=>t.json()).then(t=>{const o=document.querySelector("#hitokoto_text");o.href=`https://hitokoto.cn/?uuid=${t.uuid}`,o.innerText=t.hitokoto}).catch(console.error);
