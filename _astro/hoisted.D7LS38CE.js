6dfdf6b92ed95e1363d3ce3734aa65216205a494
2kindlebox <2kindlebox@gmail.com>
2026-02-19 03:01:13 +0000

    Deploying to gh-pages from  @ 1c771e607017a2ea5451f84fcf643f76e60cc079 🚀

---
+++ _astro/hoisted.D7LS38CE.js
@@ -0,0 +1,1 @@
+import"./hoisted.Cb6je_wm.js";import"./auth.BHwicO5t.js";fetch("https://v1.hitokoto.cn").then(t=>t.json()).then(t=>{const o=document.querySelector("#hitokoto_text");o.href=`https://hitokoto.cn/?uuid=${t.uuid}`,o.innerText=t.hitokoto}).catch(console.error);
